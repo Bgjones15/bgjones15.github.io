12abef94f78f141cd0b6c4c3985de6357f41a87f
Barett <barettj22@gmail.com>
2021-03-20 16:49:34 -0700

    Adding styling and remove left nav

--- script/script.js
+++ script/script.js
@@ -3,16 +3,27 @@ $(function() {
     if (document.location.hash == "" || document.location.hash == "#") {
         document.location.hash = "#home";
     };
+
+    replaceSymbols(["<",">","/"]);
         
 
-    var app = document.getElementById('home');
+    var app = document.getElementById('name');
 
 var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('Hey!')
+typewriter.typeString('Barett!')
     .pauseFor(2500)
     .start();
-
 });
+
+function replaceSymbols(symbols){
+    symbols.forEach(n => {
+        $('body :not(script)').contents().filter(function() {
+            return this.nodeType === 3;
+          }).replaceWith(function() {
+              return this.nodeValue.replace(n,`<span class="symbol">${n}</span>`);
+          });
+    });
+}
