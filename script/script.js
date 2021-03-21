16075bebcbb0c7fa1a51b3a99b4f23b1f0fec6b1
Barett <barettj22@gmail.com>
2021-03-20 19:28:46 -0700

    Added minesweeper

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+var board;
 $(function() {
 
     if (document.location.hash == "" || document.location.hash == "#") {
@@ -6,16 +7,20 @@ $(function() {
 
     replaceSymbols(["<",">","/"]);
         
+    board = $('#gameboard');
+    init()
 
-    var app = document.getElementById('name');
+    var app = document.getElementById("name");
 
-var typewriter = new Typewriter(app, {
-    loop: true
-});
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
 
-typewriter.typeString('Barett!')
-    .pauseFor(2500)
-    .start();
+    typewriter.typeString('Barett!')
+        .pauseFor(2500)
+        .start();
+
+    
 });
 
 function replaceSymbols(symbols){
@@ -27,3 +32,98 @@ function replaceSymbols(symbols){
           });
     });
 }
+
+function init(){
+    for (let i = 0; i < 10; i++) {
+        board.append($(`<div id="row${i}" class="row"></div>`));
+        for (let j = 0; j < 10; j++) {
+            if(Math.floor(Math.random() * 5) == 1){
+                $(`#row${i}`).append($(`<button id='${i}-${j}' class='gamebutton bomb'>`));
+            }
+            else{
+                $(`#row${i}`).append($(`<button id='${i}-${j}' class='gamebutton safe'>`));
+            }
+        }
+    }
+
+    generateNumbers();
+    $(".gamebutton").click(function() {
+        userClick($(this));
+    });
+
+    $(document).on("contextmenu", ".gamebutton", function(e){
+        userRightClick($(this));
+        return false;
+     });
+}
+
+function generateNumbers(){
+    $(".safe").each(function() {
+        let numbers = $(this)[0].id.split("-");
+        column = numbers[0];
+        row = numbers[1];
+        let adjacents = generateAdjacent(column, row, 9);
+        let bombs = 0;
+        console.log(column, row);
+        adjacents.forEach(a => {
+            if($(`#${a}`)[0].className.includes("bomb")){
+                bombs++;
+            }
+        });
+        $(this).addClass(""+bombs);
+    });
+}
+
+function generateAdjacent(x, y, size){
+    let adjacent = [];
+    //top row
+    if(x!=0){
+        adjacent.push(`${parseInt(x)-1}-${parseInt(y)}`);
+        if(y>0){
+            adjacent.push(`${parseInt(x)-1}-${(parseInt(y)-1)}`);
+        }
+        if(y<size){
+            adjacent.push(`${parseInt(x)-1}-${parseInt(y)+1}`);
+        }
+    }
+    //left, right
+    if(y>0){
+        adjacent.push(`${parseInt(x)}-${parseInt(y)-1}`);
+    }
+    if(y<size){
+        adjacent.push(`${parseInt(x)}-${parseInt(y)+1}`);
+    }
+
+    //bottom row
+    if(x!=size){
+        adjacent.push(`${parseInt(x)+1}-${parseInt(y)}`);
+        if(y>0){
+            adjacent.push(`${parseInt(x)+1}-${parseInt(y)-1}`);
+        }
+        if(y<size){
+            adjacent.push(`${parseInt(x)+1}-${parseInt(y)+1}`);
+        }
+    }
+    return adjacent;
+}
+
+function userClick(button){
+    if(button[0].className.includes("bomb")){
+        alert("Boom");
+    }
+    else{
+        button.text(button[0].className[button[0].className.length - 1]);
+    }
+}
+
+function userRightClick(button){
+    console.log($(button).css('background'));
+    if($(button).css('background').includes("rgb(255, 0, 0)")){
+        $(button).css('background', '');
+    }
+    else{
+        $(button).css('background', 'red');
+    }
+}
+
+
