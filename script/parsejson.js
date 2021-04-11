22e409134bf5b5b8a2c4b8b5d5c839dddd0689b2
Barett <bgjones15@winona.edu>
2021-04-11 10:07:08 -0500

    Changed append

--- script/parsejson.js
+++ script/parsejson.js
@@ -2,7 +2,7 @@ $(function() {
     $.getJSON( "./items.json", function( data ) {
     var items = [];
     $.each( data, function( key, val ) {
-        $("#list").append(`<li>${val.item} ${val.description?"":val.description} ${val.link}</li>`)
+        $("#list").append(`<li>${val.item} ${val.description} <a href="${val.link}">link</a></li>`);
     });
   });
 });
