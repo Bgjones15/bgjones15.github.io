07918c55e8591a5a31e8ebc983c2e015f23e6c13
Bgjones15 <bgjones15@winona.edu>
2021-07-21 19:26:09 -0500

    Delete script directory

--- script/parsejson.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $.getJSON( "./items.json", function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-        $("#list").append(`<li>${val.item} ${val.description} <a href="${val.link}">link</a></li>`);
-    });
-  });
-});
